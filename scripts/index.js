c8c4e68780e99182bf32570ca1feadd49be2b21b
Sergey <isdznv@yandex.ru>
2023-07-09 22:28:23 +0300

    fix: исправлены ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,18 @@ let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('.form__item_el_name');
 let jobInput = formElement.querySelector('.form__item_el_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__activity');
 
 
 function openPopup() {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openPopup);
@@ -20,11 +24,10 @@ closeButton.addEventListener('click', closePopup);
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__activity');
-
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    
+    closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
